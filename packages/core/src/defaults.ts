42861e5826f85863413a24f39471cd67d42887f6
João Lucas <jlucaso@hotmail.com>
2025-04-20 15:07:58 -0300

    chore: update NOISE_WA_HEADER version to 3 and contants binary

--- packages/core/src/defaults.ts
+++ packages/core/src/defaults.ts
@@ -4,7 +4,7 @@ export const WA_VERSION = [2, 3000, 1021636778];
 export const DEFAULT_BROWSER = ["Wha.ts", "Desktop", "0.1"] as const;
 
 export const NOISE_MODE = "Noise_XX_25519_AESGCM_SHA256\0\0\0\0";
-export const NOISE_WA_HEADER = new Uint8Array([87, 65, 6, 2]);
+export const NOISE_WA_HEADER = new Uint8Array([87, 65, 6, 3]);
 
 export const DEFAULT_SOCKET_CONFIG = {
 	waWebSocketUrl: "wss://web.whatsapp.com/ws/chat",
